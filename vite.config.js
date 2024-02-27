cb8840e3dd3639562d57744035c08a9cbd226177
deflis <deflis@gmail.com>
2024-02-27 22:34:38 +0900

    Update Node.js setup and add coverage report action

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
+  }
+});
