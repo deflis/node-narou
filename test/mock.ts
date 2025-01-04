ded56ea019eb8f447ab02801ffa24b2efaf70393
deflis <deflis@gmail.com>
2025-01-05 02:15:40 +0900

    responseGzipOrJson関数の型引数を追加し、より厳密な型チェックを実現

--- test/mock.ts
+++ test/mock.ts
@@ -1,7 +1,7 @@
-import { HttpResponse } from "msw";
+import { HttpResponse, JsonBodyType } from "msw";
 import { gzipSync } from "zlib";
 
-export function responseGzipOrJson(response: any, url: URL) {
+export function responseGzipOrJson<T extends JsonBodyType>(response: T, url: URL) {
   if (parseInt(url.searchParams.get("gzip") ?? "0") > 0) {
     return HttpResponse.arrayBuffer(
       gzipSync(Buffer.from(JSON.stringify(response)))
