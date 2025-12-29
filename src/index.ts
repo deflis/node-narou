ebd7c08aa8c3880e8cdbea0f91c1e27af270e00c
deflis <deflis@gmail.com>
2025-12-29 21:46:53 +0900

    なろう小説APIの実行オプションを追加
    - ExecuteOptionsインターフェースを追加
    - 各API実行メソッドにoptions引数を追加
    - NarouNovelFetchのfetchメソッドにfetchOptionsを渡すテストを追加

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import type NarouNovel from "./narou.js";
+import type { ExecuteOptions } from "./narou.js";
 import NarouNovelFetch from "./narou-fetch.js";
 import NarouNovelJsonp from "./narou-jsonp.js";
 import RankingBuilder from "./ranking.js";
@@ -66,14 +67,17 @@ export function ranking(api: NarouNovel = narouNovelFetch): RankingBuilder {
 
 /**
  * なろう殿堂入り API でランキング履歴を取得する
- * @param {string} ncode 小説のNコード
+ * @param ncode 小説のNコード
+ * @param options 実行オプション
+ * @param api API実行クラスのインスタンス
  * @see https://dev.syosetu.com/man/rankinapi/
  */
 export async function rankingHistory(
   ncode: string,
+  options?: ExecuteOptions,
   api: NarouNovel = narouNovelFetch
 ): Promise<RankingHistoryResult[]> {
-  const result = await api.executeRankingHistory({ ncode });
+  const result = await api.executeRankingHistory({ ncode }, options);
   if (Array.isArray(result)) {
     return result.map(formatRankingHistory);
   } else {
